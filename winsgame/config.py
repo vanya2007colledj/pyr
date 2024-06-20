1164bff9e475d37594a8ea327f7018ad85fc3e1c
vanya2007colledj <xasbyla2001@gmail.com>
2024-06-21 02:51:12 +0300

    Add files via upload

---
+++ winsgame/config.py
@@ -0,0 +1,8 @@
+TOKEN = "7458031784:AAHK6zDfEdecYuUhICLa4CLlMeS3z-jjU24" #токен BotFather
+CRYPTO_TOKEN = "13680:AAV3vNampurjzUvxIhKk44MebTY3wyPSFzf" #токен CreptoPay
+LOG_CHANNEL = -1002152337746 #ЛОГИРОВАНИЕ
+CHANNEL_BROKER = -1002245590099 #Посредник
+MAIN_CHANNEL = -1002245590099 #основа
+ADMINS = [6970609444, 680687451] #список админов
+CHECK_URL = "http://t.me/CryptoTestnetBot?start=IVH1jisSVOXe" #юрл счёта
+MONEYBACK = 0 #процент манибэка от игры
---
+++ winsgame/settings/coefs.py
@@ -0,0 +1,18 @@
+#КЭФЫ
+
+DICE = 1.8 #ЧЕТ НЕЧЕТ КУБ
+DICE_MORE_LESS = 1.8#БОЛЬШЕ МЕНЬШЕ
+DICE_NUMBER = 4 #ЧИСЛО
+DUEL = 1.8 #ДУЭЛЬ
+DARTS = 4 #ДАРТС
+DARTS_COLOR = 1.8 #ДАРТС КРАСН/БЕЛ
+BASKET = 1.8 #БАСКЕТ
+BASKET_MISS = 1.25 #БАСКЕТ МИМО
+FOOTBALL = 1.25 #ФУТБОЛ
+FOOTBALL_MISS = 1.8#ФУТБОЛ МИМО
+BOWLING = 4 #БОУЛИНГ
+SLOTS_777 = 10 #СЛОТЫ 777
+SLOTS_GRAPE = 5 #СЛОТЫ ВИНОГРАД
+SLOTS_BAR = 5 #СЛОТЫ БАР
+SLOTS_LEMON = 5 #СЛОТЫ ЛИМОН
+KNB = 1.8 #КАМЕНЬ НОЖНИЦЫ БУМАГА
